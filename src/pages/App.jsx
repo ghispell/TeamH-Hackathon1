fcabeee383be06dcf384c8df17c324c4c4e650f1
Baris G <barisg@pm.me>
2023-11-23 18:42:45 +0100

    bouton fonctionel

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,23 +1,8 @@
-import { useState, useEffect } from "react";
 import Home from "../components/Home";
 import backgroundVideo from "../assets/video_hackathon.mp4";
 import BoutonList from "../components/BoutonList";
 import QuizFrame from "../components/QuizFrame";
 function App() {
-  const [timer, setTimer] = useState(30);
-
-  useEffect(() => {
-    const chrono = setTimeout(() => {
-      if (timer > 0) {
-        setTimer((prev) => prev - 1);
-      } else {
-        // ici il faudra ajouter ce qui se passe quand on arrive à 0 seconde sur le chrono, soit on passe direct à la prochaine question, qoit peut etre qu'on laisse le mec cliquer sur next ¯\_(ツ)_/¯
-      }
-    }, 1000);
-
-    return () => clearTimeout(chrono);
-  }, [timer]);
-
   return (
     <div>
       <Home />
