4a921a2617edf298e3232777c696a18046802727
Ijuaa <pierre.colpart@gmail.com>
2023-11-23 14:11:46 +0100

    add timer

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+import filmsNoel from "../Data/data.json";
+
 function App() {
-	return (
-		<div>
-			<p>Template SACOD</p>
-		</div>
-	);
+  const [timer, setTimer] = useState(30);
+  console.log(filmsNoel);
+
+  useEffect(() => {
+    const chrono = setTimeout(() => {
+      if (timer > 0) {
+        setTimer((prev) => {
+          prev = prev - 1;
+        });
+      } else {
+        // ici il faudra ajouter ce qui se passe quand on arrive à 0 seconde sur le chrono, soit on passe direct à la prochaine question, qoit peut etre qu'on laisse le mec cliquer sur next ¯\_(ツ)_/¯
+      }
+    }, 1000);
+
+    return () => clearTimeout(chrono);
+  }, [timer]);
+  return (
+    <div>
+      <p>Template SACOD</p>
+    </div>
+  );
 }
 
 export default App;
